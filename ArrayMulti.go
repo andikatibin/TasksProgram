da13a1fe244a5e14d6f4840934146c719c35abb5
program/AndiKatibin <95963247+andikatibin@users.noreply.github.com>
2022-02-01 00:31:56 +0800

    Update ArrayMulti.go

--- ArrayMulti.go
+++ ArrayMulti.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
   fmt.Println("Array Multi Dimensi")
-  var angka = [5][2]int{{0, 0}, {2, 2}, {3, 4}, {5, 6}, {7, 8}}
+  var angka = [10][2]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 10}, {10, 11}}
   
-  for i := 0; i < 5; i++ {
+  for i := 0; i < 10; i++ {
   for j := 0; j < 2; i++ {
     
-  fmt.Printf("Date Ke [%d][%d] = %d\n", i, j, angka [i][j])
+  fmt.Printf("[%d][%d] = %d\n", i, i, j, angka [i][j])
   }
   }
 }
