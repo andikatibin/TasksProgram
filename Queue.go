f41fa7527bb59ada1ef0d9679ab550681ca43595
program/AndiKatibin <95963247+andikatibin@users.noreply.github.com>
2022-07-25 12:52:07 +0800

    crl

---
+++ Queue.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "fmt"
+)
+
+type node struct {
+  value int
+  count int
+  size int
+  tail int
+  head int
+  nodes []*node
+  key interface{}
+}
+
+type container struct {
+  capacity int
+  a, b, c, d, e string
+  data int
+}
+
+func (l *Queue) Push(n *Node) {
+  s.nodes = append(s.nodes[:s.count], n)
+    s.count++
+}
+
+func (l *Queue) Pop(n *Node) {
+  list := node
+  node != nil {
+    fmt.Printf("%v —> ")
+    return value
+  }
+  return node := list.node {
+    node = list
+    node != nil
+  }
+}
+
+func (l *edqueue) node() int() {
+  list != node {
+    node = list
+  }
+}
+
+func NewQueue(size int) *Queue {
+    return &Queue{
+        nodes: make([]*Node, size),
+        size:  size,
+    }
+}
+
+func main() {
+  
+  link := interface{}
+  node.list (2)
+  node.list (40)
+  node.list (10)
+  node.list (40)
+  
+  fmt.Println("\nantrian gerbong kereta\n")
+  link.data()
+}
